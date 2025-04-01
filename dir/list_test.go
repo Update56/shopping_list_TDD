9d2a5984ad649829347e4ed698957dbf1ff24a75
Update56 <heupdate56@gmail.com>
2025-04-01 23:38:23 +0700

    Added shopping_list struct. Shopping list creation test successes

--- dir/list_test.go
+++ dir/list_test.go
@@ -6,6 +6,6 @@ import "testing"
 func TestSendMessage(t *testing.T) {
 	var shp_ls shopping_list
 	if &shp_ls == nil {
-		t.Errorf("Экземпляр типа не создан")
+		t.Errorf("Экземпляр структуры не создан")
 	}
 }
