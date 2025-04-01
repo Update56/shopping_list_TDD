c0e77c717c23472a8d498aba46dd496ecacdd1ed
Update56 <heupdate56@gmail.com>
2025-04-01 23:32:45 +0700

    Shopping list creation test

--- dir/list_test.go
+++ dir/list_test.go
@@ -1,1 +1,11 @@
 package list
+
+import "testing"
+
+//Тест на создание списка
+func TestSendMessage(t *testing.T) {
+	var shp_ls shopping_list
+	if &shp_ls == nil {
+		t.Errorf("Экземпляр типа не создан")
+	}
+}
