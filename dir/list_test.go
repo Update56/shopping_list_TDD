8b720951691e85a0d136db831df424367659b160
Update56 <heupdate56@gmail.com>
2025-04-19 20:22:57 +0700

    Shop list creation test

--- dir/list_test.go
+++ dir/list_test.go
@@ -1,1 +1,11 @@
 package list
+
+import "testing"
+
+//Тест на создание списка
+func TestShopListCreation(t *testing.T) {
+	var shp_ls shopping_list
+	if &shp_ls == nil {
+		t.Errorf("Экземпляр структуры не создан")
+	}
+}
