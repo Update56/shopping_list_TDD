9f61d1b586eb68ff3b08da088c0c52f3edbb40f0
Update56 <heupdate56@gmail.com>
2025-04-19 20:58:19 +0700

    Added Shop List Slice type. Shop List Slice creation test successes

--- dir/list.go
+++ dir/list.go
@@ -1,7 +1,11 @@
 package list
 
+//структура элмента списка
 type shoppingListItem struct {
 	Name   string  //продукт
 	Amount float64 //количество
 	Unit   string  //единица измерений (шт, кг, л и т.д.)
 }
+
+//срез из элементов списка
+type ShoppingList []shoppingListItem
