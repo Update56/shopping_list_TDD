b21930c2416cd25131df971290089d56cc505a9d
Update56 <heupdate56@gmail.com>
2025-04-19 23:37:55 +0700

    Add method for adding item from instance into slice

--- dir/list.go
+++ dir/list.go
@@ -12,5 +12,12 @@ type ShoppingList []shoppingListItem
 
 //функция добавления элемента
 func (s ShoppingList) addItem(name string, amount float64, unit string) shoppingListItem {
+	//todo реализовать метод addItem позднее до конца
 	return shoppingListItem{Name: "Milk", Amount: 4.5, Unit: "L"}
+}
+
+//функция добавления элемента из экзепляра
+func (s ShoppingList) addItemFromInst(item shoppingListItem) shoppingListItem {
+	//todo реализовать метод addItemFromInst позднее до конца
+	return shoppingListItem{Name: "Sugar", Amount: 10, Unit: "kg"}
 }
