d329826b2751eb6cbd01e9a70f005ff93c4f43ae
Update56 <heupdate56@gmail.com>
2025-04-19 20:30:11 +0700

    Test check access to fields of struct

--- dir/list_test.go
+++ dir/list_test.go
@@ -9,3 +9,17 @@ func TestShopListCreation(t *testing.T) {
 		t.Errorf("Экземпляр структуры не создан")
 	}
 }
+
+//Тест проверку полей структуры списка
+func TestShopListFields(t *testing.T) {
+	var shp_ls shopping_list
+	if &shp_ls.Name == nil {
+		t.Errorf("Поле \"Name\" не существует")
+	}
+	if &shp_ls.Amount == nil {
+		t.Errorf("Поле \"Amount\" не существует")
+	}
+	if &shp_ls.Unit == nil {
+		t.Errorf("Поле \"Unit\" не существует")
+	}
+}
